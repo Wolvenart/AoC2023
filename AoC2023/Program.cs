73adaafee29c7de0de5a47e0b20a2a72591a0289
Wolvenart <wolvenart@gmail.com>
2023-12-01 23:15:56 +0300

    initial commit.
    Day 1

---
+++ AoC2023/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(Day_01.Main.Part1(true));
+Console.WriteLine(Day_01.Main.Part2(false));
---
+++ Day_01/Day_01.cs
@@ -0,0 +1,91 @@
+﻿namespace Day_01
+{
+    public static class Main
+    {
+        private static readonly Dictionary<string, int> NumText = new Dictionary<string, int>()
+        {
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+        };
+        public static int Part1(bool test)
+        {
+            var input = test ? Properties.Resources.TestInput : Properties.Resources.RealInput;
+
+            List<int> lstVals = new List<int>();
+            foreach (var line in input.Split("\r\n"))
+            {
+                var valTemp = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                    if (Int32.TryParse(Convert.ToString(line[i]), out var val))
+                    {
+                        valTemp = val * 10;
+                        break;
+                    }
+                for (int i = line.Length - 1; i >= 0; i--)
+                    if (Int32.TryParse(Convert.ToString(line[i]), out var val))
+                    {
+                        valTemp += val;
+                        break;
+                    }
+
+                lstVals.Add(valTemp);
+            }
+            return lstVals.Sum();
+        }
+        public static int Part2(bool test)
+        {
+            var input = test ? Properties.Resources.TestInput2 : Properties.Resources.RealInput;
+
+            var lstVals = new List<int>();
+            foreach (var line in input.Split("\r\n"))
+            {
+                var valTemp = 0;
+
+                var strTempVal = String.Empty;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (NumText.Any(x => strTempVal.Contains(x.Key)))
+                    {
+                        var keyCheck = NumText.FirstOrDefault(x => strTempVal.Contains(x.Key)).Value;
+                        valTemp = keyCheck * 10;
+                        break;
+                    }
+                    if (Int32.TryParse(Convert.ToString(line[i]), out var val))
+                    {
+                        valTemp = val * 10; 
+                        break;
+                    }
+                    strTempVal += line[i];
+                }
+
+                strTempVal = String.Empty;
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    if (NumText.Any(x => new string(strTempVal.Reverse().ToArray()).Contains(x.Key)))
+                    {
+                        var keyCheck = NumText.FirstOrDefault(x => new string(strTempVal.Reverse().ToArray()).Contains(x.Key)).Value;
+                        valTemp += keyCheck;
+                        break;
+                    }
+                    if (Int32.TryParse(Convert.ToString(line[i]), out var val))
+                    {
+                        valTemp += val;
+                        break;
+                    }
+                    strTempVal += line[i];
+                }
+                lstVals.Add(valTemp);
+            }
+
+            return lstVals.Sum();
+        }
+    }
+}
